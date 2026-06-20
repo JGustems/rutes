7e18ac0de0264d67a0ed4d13d329f6fc3db134d1
JGustems <smetsug@gmail.com>
2026-06-20 23:02:28 +0200

    Update all-routes-map.tsx

--- src/components/all-routes-map.tsx
+++ src/components/all-routes-map.tsx
@@ -308,11 +308,11 @@ export default function AllRoutesMap({ rutes }: { rutes: RutaAmbTrack[] }) {
 
       <style jsx global>{`
         .popup-translucid .leaflet-popup-content-wrapper {
-          background: rgba(255, 255, 255, 0.88);
+          background: rgba(255, 255, 255, 0.78);
           backdrop-filter: blur(2px);
         }
         .popup-translucid .leaflet-popup-tip {
-          background: rgba(255, 255, 255, 0.88);
+          background: rgba(255, 255, 255, 0.78);
         }
       `}</style>
     </div>
